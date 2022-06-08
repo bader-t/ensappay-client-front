c8b1dd43a5c6c0c289b15dba3f639db5fbf3bbfb
hamza <hitec.appdev@gmail.com>
2022-06-08 20:36:11 +0100

    added history and creditors

--- src/app/account/components/history/history.component.ts
+++ src/app/account/components/history/history.component.ts
@@ -9,6 +9,12 @@ export class HistoryComponent implements OnInit {
 
   constructor() { }
 
+
+  MarocTLC_URL:string = "https://halberdbastion.com/sites/default/files/2017-08/Maroc-Telecom-Logo.png";
+  Inwi_URL:string = "https://www.marocemploi.cc/storage/files/ma/68/thumb-816x460-369c557388a7b34f87e6ae42fe50d9d6.jpg";
+  Orange_URL:string = "https://www.adipsys.com/wp-content/uploads/2018/11/Logo-Orange-emea.png";
+
+
   ngOnInit(): void {
   }
 
