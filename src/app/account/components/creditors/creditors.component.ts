a27903be74189a8a008ba9af8b23bf3a5a28be93
Bader Toumi <55552093+bader-t@users.noreply.github.com>
2022-06-09 12:38:31 +0100

    added more modifications into providers and creditors

--- src/app/account/components/creditors/creditors.component.ts
+++ src/app/account/components/creditors/creditors.component.ts
@@ -14,10 +14,6 @@ export class CreditorsComponent implements OnInit {
 
   constructor(private providerService: ProviderService, private alertService: AlertService) { }
 
-  MarocTLC_URL: string = "https://halberdbastion.com/sites/default/files/2017-08/Maroc-Telecom-Logo.png";
-  Inwi_URL: string = "https://www.marocemploi.cc/storage/files/ma/68/thumb-816x460-369c557388a7b34f87e6ae42fe50d9d6.jpg";
-  Orange_URL: string = "https://www.adipsys.com/wp-content/uploads/2018/11/Logo-Orange-emea.png";
-
 
   ngOnInit(): void {
     this.getAllProviders();
@@ -33,7 +29,6 @@ export class CreditorsComponent implements OnInit {
       },
       complete: () => {
       }
-
     })
   }
 
