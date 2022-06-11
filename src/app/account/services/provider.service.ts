38ba73cf63a287af58dd8eb2ae961fb9820846c8
Bader Toumi <55552093+bader-t@users.noreply.github.com>
2022-06-12 00:27:47 +0100

    added getCreditors service that consumes soap request

--- src/app/account/services/provider.service.ts
+++ src/app/account/services/provider.service.ts
@@ -2,8 +2,6 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Provider } from '../models/provider.model';
-
-
 const providerUrl = 'http://localhost:8080/api/providers';
 
 @Injectable({
@@ -50,6 +48,42 @@ export class ProviderService {
     // return this.http.get<Provider[]>(providerUrl);
     return of(this.providers);
   }
+
+  getCreditors(): void {
+    const xmlhttp = new XMLHttpRequest();
+    xmlhttp.open('POST', 'http://localhost:8080/cmi-soap/creanciersList', true);
+    const sr =
+      `<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:cre="http://www.ebanking.ensa/accountservice/Soap/Request/CreanciersList/">
+            <soapenv:Header/>
+            <soapenv:Body>
+                <cre:CreanciersListRequest>?</cre:CreanciersListRequest>
+            </soapenv:Body>
+          </soapenv:Envelope>`;
+
+    xmlhttp.onreadystatechange = () => {
+      if (xmlhttp.readyState == 4) {
+        if (xmlhttp.status == 200) {
+
+          const creanciers = xmlhttp.responseXML?.getElementsByTagName('ns2:creancier');
+          for (let i = 0; i < creanciers?.length!; i++) {
+            console.log(xmlhttp.responseXML?.getElementsByTagName('ns2:creancier').item(i)?.childNodes[0].textContent)
+            console.log(xmlhttp.responseXML?.getElementsByTagName('ns2:creancier').item(i)?.childNodes[1].textContent)
+            console.log(xmlhttp.responseXML?.getElementsByTagName('ns2:creancier').item(i)?.childNodes[2].textContent)
+            console.log(xmlhttp.responseXML?.getElementsByTagName('ns2:creancier').item(i)?.childNodes[3].childNodes[0].textContent)
+            console.log(xmlhttp.responseXML?.getElementsByTagName('ns2:creancier').item(i)?.childNodes[3].childNodes[1].textContent)
+            console.log('---------')
+
+          }
+        }
+      }
+    }
+    console.log("before send")
+    xmlhttp.setRequestHeader('Content-Type', 'text/xml');
+    xmlhttp.send(sr);
+  }
+
+
+
 
   findBySurname(surname: String | undefined): Provider | undefined {
     return this.providers.find(provider => provider.surname == surname);
