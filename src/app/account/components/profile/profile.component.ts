4106f6b7e767ce39d2acef6cff7f5688fc317373
Bader Toumi <55552093+bader-t@users.noreply.github.com>
2022-06-26 23:00:08 +0100

    add account balance

--- src/app/account/components/profile/profile.component.ts
+++ src/app/account/components/profile/profile.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { TokenStorageService } from 'src/app/auth/services/token-storage.service';
+import { ProviderService } from '../../services/provider.service';
 
 @Component({
   selector: 'app-profile',
@@ -8,14 +9,24 @@ import { TokenStorageService } from 'src/app/auth/services/token-storage.service
 })
 export class ProfileComponent implements OnInit {
 
-   client: any;
+  client: any;
+  balance: string = "";
 
-   constructor(private authStorageService: TokenStorageService) {
-     
+  constructor(private authStorageService: TokenStorageService, private providerService: ProviderService) {
+
     this.client = this.authStorageService.getClient();
   }
 
   ngOnInit(): void {
+    this.providerService.consultBalance(this.authStorageService.getPhoneNumber()!).subscribe({
+      next: (data: any) => {
+        this.balance = data;
+      },
+      error: (e: any) => {
+      },
+      complete: () => {
+      }
+    })
   }
 
 }
